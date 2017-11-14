cdd4a4c5a3437cbb57fd557312ae65d491b884b3
sdanilv <sdanilv@gmail.com>
2017-11-14 19:42:52 +0200

    new

---
+++ src/test/java/ua/my/services/ConferenceServiceImplTest.java
@@ -0,0 +1,31 @@
+package ua.my.services;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ua.my.model.Conference;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ua.my.config.AppConfig.class})
+@WebIntegrationTest
+public class ConferenceServiceImplTest {
+
+    @Autowired
+    private ConferenceService conferenceService;
+
+    @Test
+    public void addConferenceTest(){
+//        conferenceService.addConference(new Conference(null, "a","a",1, date, "", "a"));
+        System.out.println(conferenceService.searchConferences("a"));
+    }
+
+
+
+}
